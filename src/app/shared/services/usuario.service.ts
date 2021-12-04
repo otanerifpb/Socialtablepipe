2c16d13a11506aba5c7c3681d1f87e393c7b0977
otanerifpb <renato.silva.pro@gmail.com>
2021-12-03 22:18:37 -0300

    Create usuario.service.ts

---
+++ src/app/shared/services/usuario.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {Usuario} from '../model/usuario';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  URL_USUARIOS = 'http://localhost:3000/usuarios';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  listar(): Observable<Usuario[]> {
+    // @ts-ignore
+    return this.httpClient.get<Usuario[]>(this.URL_USUARIOS).pipe(catchError(erro => {
+      console.log('Erro:', erro);
+      return of(undefined);
+    }));
+  }
+
+  // listar(): Observable<Usuario[]> {
+  //   return this.httpClient.get<Usuario[]>(this.URL_USUARIOS);
+  // }
+
+  inserir(usuario: Usuario): Observable<Usuario> {
+    return this.httpClient.post<Usuario>(this.URL_USUARIOS, usuario);
+  }
+
+  remover(id: number): Observable<object> {
+    return this.httpClient.delete(`${this.URL_USUARIOS}/${id}`);
+  }
+
+  pesquisarPorId(id: number): Observable<Usuario> {
+    return this.httpClient.get<Usuario>(`${this.URL_USUARIOS}/${id}`);
+  }
+
+  atualizar(usuario: Usuario): Observable<Usuario> {
+    return this.httpClient.put<Usuario>(`${this.URL_USUARIOS}/${usuario.id}`, usuario);
+  }
+}
