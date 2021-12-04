57b7f2bb5158ac382db13fa68df463c0ad4b9355
otanerifpb <renato.silva.pro@gmail.com>
2021-12-03 22:18:07 -0300

    Create pipes.module.ts

---
+++ src/app/shared/pipes/pipes.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LetraMaiusculaPipe} from './letra-maiuscula.pipe';
+import { CpfPipe } from './cpf.pipe';
+import { TelefonePipe } from './telefone.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    LetraMaiusculaPipe,
+    CpfPipe,
+    TelefonePipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LetraMaiusculaPipe,
+    CpfPipe,
+    TelefonePipe
+  ]
+})
+export class PipesModule { }
